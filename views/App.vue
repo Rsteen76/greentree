1526df915f4728d27d3e32f2da288ffdc7c2a9d8
Ryan Steen <ryansteen76@gmail.com>
2018-10-08 15:09:18 -0700

    modified home page

--- views/App.vue
+++ views/App.vue
@@ -1,158 +1,105 @@
 <template>
-  <v-app :dark="isDarkMode">
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      app
-      width="200"
-      class="primary"
-    >
-      <v-list>
-        <v-list-tile 
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-          :href="item.href"
-          :class="getNavClass(item.href)"
-        >
-          <v-list-tile-action>
-            <v-icon class="primaryText--text" v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="primaryText--text" v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-toolbar class="primary primaryText--text" fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon class="primaryText--text" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon class="primaryText--text" v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      
-      <v-toolbar-title> {{ title }} </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn @click="isDarkMode = !isDarkMode" icon>
-        <v-icon class="primaryText--text" v-if="!isDarkMode" > brightness_5 </v-icon>
-        <v-icon class="primaryText--text" v-else > brightness_3 </v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    
-    <main>
-      <v-content>
-        <v-container fluid >
-            <v-layout column align-center>
-              <v-fade-transition mode="out-in">
-              <router-view @alert="alert" ></router-view>
-            </v-fade-transition>
-            </v-layout>
-        </v-container>
-      </v-content>
-    </main>
-    
-    <v-footer :fixed="fixed" app>
-      <span> Template created by <a class="lightText--text" href="http://github.com/aturingmachine">Vincent Blom</a></span>
-    </v-footer>
-
-    <v-snackbar
-    bottom
-    :value="alertOpen"
-    :color="alertSuccess ? 'success' : 'error'"
-     >
-     {{ alertString }}
-    </v-snackbar>
-  </v-app>
+  <div class="home">
+    <Hero2 />
+    <Nav />
+    <div class="bgimg-1">
+      <div>
+        <About />
+      </div>
+    </div>
+    <div class="bgimg-2">
+      <div>
+        <Quotes />
+      </div>
+    </div>
+    <div class="bgimg-3">
+      <div>
+        <Children />
+      </div>
+    </div>
+    <div class="bgimg-1">
+      <div>
+        <Calendar />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import Nav from '../views/components/Nav.vue'
+import Hero2 from '../views/components/Hero2.vue'
+import About from '../views/components/About.vue'
+import Quotes from '../views/components/Quotes.vue'
+import Children from '../views/components/Children.vue'
+import Calendar from '../views/components/Calendar.vue'
+
 export default {
-  data: () => {
-    return {
-      clipped: true,
-      drawer: true,
-      fixed: false,
-      items: [
-        {
-          icon: "home",
-          title: "Home",
-          href: "/#/home",
-          router: true
-        },
-        {
-          icon: "account_circle",
-          title: "Users",
-          href: "/#/users",
-          router: true
-        },
-        {
-          icon: "calendar_today",
-          title: "Schedule",
-          href: "/#/schedules",
-          router: true
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Green Tree",
-      alertOpen: false,
-      alertString: '',
-      alertSuccess: false,
-      isDarkMode: false,
-    };
-  },
-
-  watch: {
-    isDarkMode() {
-      if (!this.isDarkMode) { //NOT dark mode
-        this.$vuetify.theme = {
-          primary: '#0D47A1', //Main color
-          primaryText: '#FFFFFF', //Color for text on primary
-          secondary: '#90CAF9', //Color for active nav
-          lightText: '#000000'
-        }
-      } else { //Dark mode
-        this.$vuetify.theme = {
-          primary: '#424242', //Main color
-          primaryText: '#FFFFFF', //Color for text on primary
-          secondary: '#90CAF9', //Color for active nav
-          lightText: '#FFFFFF'
-        }
-      }
-    }
-  },
-
-  methods: {
-    alert(success, callName, resource) {
-      this.alertOpen = false
-      this.alertString = callName + ' ' + resource
-      if (success) {
-        this.alertSuccess = true
-        this.alertString += ' was successful.'
-      } else {
-        this.alertSuccess = false
-        this.alertString += ' has failed.'
-      }
-      this.alertOpen = true
-    },
-
-    getNavClass(href) {
-      if (this.$route.fullPath == href.substring(2)) {
-        return 'secondary primaryText--text'
-      } else {
-        return 'primary primaryText--text'
-      }
-    }
+  name: 'home',
+  components: {
+    Nav,
+    Hero2,
+    About,
+    Quotes,
+    Children,
+    Calendar
   }
-
-  
-};
+}
 </script>
+<style>
+.bgimg-1, .bgimg-2, .bgimg-3 {
+  position: relative;
+  opacity: 1;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+.bgimg-2 {
+  background-image: url("https://images.unsplash.com/photo-1463595709917-93fb36118407?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3e058f4f463e321effdbc2a75fdad68d&auto=format&fit=crop&w=1350&q=80");
+  min-height: 100%;
+}
+
+.bgimg-1 {
+  background-image: url("https://images.unsplash.com/photo-1440913846014-90f66ff0e334?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bef135fe947dd1212153d722e1c7b02d&auto=format&fit=crop&w=1950&q=80");
+  min-height: 400px;
+}
+
+.bgimg-3 {
+  background-image: url("https://images.unsplash.com/photo-1533234335389-ccaf6c88603d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a35a4c99f0a531c3deba29b9505867fd&auto=format&fit=crop&w=500&q=60");
+  min-height: 400px;
+}
+
+.caption {
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  color: #000;
+}
+
+.caption span.border {
+  background-color: #111;
+  color: #fff;
+  padding: 18px;
+  font-size: 25px;
+  letter-spacing: 10px;
+}
+
+h3 {
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  font: 20px "Lato", sans-serif;
+  color: #111;
+}
+
+/* Turn off parallax scrolling for tablets and phones */
+/* @media only screen and (max-device-width: 1024px) {
+    .bgimg-1, .bgimg-2, .bgimg-3 {
+        background-attachment: scroll;
+    }
+} */
+
+</style>
