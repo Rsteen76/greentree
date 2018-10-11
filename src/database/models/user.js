2d4778825156367be584c093237407db7388d3e4
Ryan Steen <ryansteen76@gmail.com>
2018-10-11 08:08:06 -0700

    Installed and configured passport

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,29 +1,49 @@
 //import mongoose, our ODM for mongoDB
 const mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-//Define all of its fields, like columns of a SQL table
-const definition = {
-  name: {
+
+
+const UserSchema = new Schema({
+  username: {
     type: String,
+    unique: true,
     required: true
   },
-  age: {
-    type: Number,
-    required: true
-  },
-  email: {
+  password: {
     type: String,
     required: true
   }
-}
+});
 
-//Set any options for the schema
-const options = {
-  timestamps: true
-}
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (this.isModified('pasword') || this.isNew) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, null, function (err, hash){
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next ();
+  } 
+});
 
-//make the schema as a new instance of a mongoose schema, using our definition and options
-const userSchema = new mongoose.Schema(definition, options)
-
+UserSchema.methods.comparePassword = function(passw, cb) {
+  bcrypt.compare(passw, this.password, function(err, isMatch) {
+    if(err) {
+         return cb(err); 
+    }
+    cb(null, isMatch);
+  });
+};
 //export that boye
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', UserSchema)
